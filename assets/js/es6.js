c93c0791e8c1b6e2792ece9220e49a9afcbe35fc
Navid Yousefzai <navid@BRM.local>
2018-06-19 10:07:45 -0700

    init

---
+++ assets/js/es6.js
@@ -0,0 +1,32 @@
+const businesss = [
+    {insurance:"iscox inc",
+    car:"alot",
+    scussed:true,
+    }, {insurance:"mike",
+    car:"toyoa",
+    scussed:true,
+    }, {insurance:"same",
+    car:"bmw",
+    scussed:true,
+    }, {insurance:"linda",
+    car:"benz",
+    scussed:true,
+    }, {insurance:"lisa",
+    car:"camry",
+    scussed:true,
+    }, {insurance:"manda",
+    car:"corolla",
+    scussed:true,
+    }
+
+]
+
+
+
+// businesss.filter(function(x){
+// console.log(x.car)
+// })
+
+
+
+console.log(businesss[0].car);
